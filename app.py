ee770aed05da967ab590dd2753eb453780410e15
Kaiser_Ali <kaiserali856@gmail.com>
2024-04-30 00:17:19 -0700

    feature added for area calculation

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from calc_function import do_addition,do_subtraction
 from multi import do_multiplication
+from area import area_triangle
 def main():
-    user_input=input("please select your choice:\n 1.add  \n 2.subtract \n 3.multiplication")
+    user_input=input("please select your choice:\n 1.add  \n 2.subtract \n 3.multiplication \n 4.area")
 
     a=int(input("first number: "))
     b=int(input("second number: "))
@@ -12,6 +13,8 @@ def main():
         result=do_subtraction(a,b)
     elif user_input=="3":
         result=do_multiplication(a,b)
+    elif user_input=="4":
+        result=area_triangle(a,b)    
     return result
 
 if __name__=="__main__":
