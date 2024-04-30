8c38aff8e92004ddee5125fe4ea56a6eee43ab59
Kaiser_Ali <kaiserali856@gmail.com>
2024-04-29 23:31:21 -0700

    this change in multiply branch

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from calc_function import do_addition,do_subtraction
+from multi import do_multiplication
 def main():
-    user_input=input("please select your choice:\n 1.add \n 2.subtract \n")
+    user_input=input("please select your choice:\n 1.add \n 2.subtract \n 3.multiplication")
 
     a=int(input("first number: "))
     b=int(input("second number: "))
 
     if user_input=="1":
         result=do_addition(a,b)
-    if user_input=="2":
+    elif user_input=="2":
         result=do_subtraction(a,b)
+    elif user_input=="3":
+        result=do_multiplication(a,b)
     return result
 
 if __name__=="__main__":
